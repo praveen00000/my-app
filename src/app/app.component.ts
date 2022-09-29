ed530e9311b8f31b89d079ec674700c13bf1bc5b
Praveen <pravee123@gmail.com>
2022-09-29 18:55:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'my-title';
+  count = 9;
+  N1=1;
+  s=0;
+  N2=6;
+  list = [12,34,13,56,32,23];
+  str = ["abc" , "def" , "ghi" , "jkl" , "mno" , "pqr" , "stu" , "vwx" , "yz"]
+
+  increment = () => {
+    this.count++;
+  }
+  decrement = () => {
+    this.count--;
+  }
+  ADD = () => {
+     this.s = this.N1 + this.N2;
+  }
 }
